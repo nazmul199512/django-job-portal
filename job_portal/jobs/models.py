8a42a6180f61911243c9001bc207ff39310b2d02
MD. Nazmul Alam <nazmul199512@gmail.com>
2020-06-23 15:26:01 +0600

    Update models.py

--- job_portal/jobs/models.py
+++ job_portal/jobs/models.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.utils import timezone
 
 
-
 class Contact(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -39,37 +38,33 @@ GENDER = (
 
 
 class JobListing(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                             null=True, editable=False, blank=True)
     title = models.CharField(max_length=100)
     company_name = models.CharField(max_length=200)
     employment_status = models.CharField(choices=JOB_TYPE, max_length=10)
     vacancy = models.CharField(max_length=10, null=True)
     gender = models.CharField(choices=GENDER, max_length=30, null=True)
-    category = models.CharField(choices=CATEGORY, max_length =30)
+    category = models.CharField(choices=CATEGORY, max_length=30)
     description = models.TextField()
     responsibilities = models.TextField()
     experience = models.CharField(max_length=100)
     job_location = models.CharField(max_length=120)
-    Salary = models.CharField(max_length=100)
+    Salary = models.CharField(max_length=100, null=True, blank=True)
     image = models.ImageField(blank=True, upload_to='media', null=True)
     application_deadline = models.DateTimeField()
     published_on = models.DateTimeField(default=timezone.now)
-
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse("jobs:job-single", args=[self.id])
-        
 
 
 class ApplyJob(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
 
-
     def __str__(self):
         return self.name
-
-        
