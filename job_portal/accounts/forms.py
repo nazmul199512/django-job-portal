97f83917b4874fbdc08dfaa6dee21e31dbb47ed5
MD. Nazmul Alam <nazmul199512@gmail.com>
2020-06-23 23:05:07 +0600

    Update forms.py

--- job_portal/accounts/forms.py
+++ job_portal/accounts/forms.py
@@ -1,14 +1,13 @@
 from django import forms
 from django.contrib.auth import authenticate, get_user_model
 
-
 User = get_user_model()
 
 
 class EmployeeRegistrationForm(forms.ModelForm):
     username = forms.CharField(max_length=10)
-    first_name = forms.CharField()
-    last_name = forms.CharField()
+    first_name = forms.CharField(label="First Name")
+    last_name = forms.CharField(label="Last Name")
     email = forms.EmailField(label='Email address')
     password = forms.CharField(widget=forms.PasswordInput)
     confirm_password = forms.CharField(widget=forms.PasswordInput)
@@ -26,10 +25,18 @@ class EmployeeRegistrationForm(forms.ModelForm):
 
         ]
 
+    def __init__(self, *args, **kwargs):
+        super(EmployeeRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter a valid e-mail'
+
 
 class EmployeeLoginForm(forms.Form):
-    username = forms.CharField(label='username')
+    username = forms.CharField(label='Username')
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super(EmployeeLoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
 
     def clean(self, *args, **kwargs):
         username = self.cleaned_data.get('username')
