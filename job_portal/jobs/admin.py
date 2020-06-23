aec0f69539d9468c437510513f1cd5b112b70428
MD. Nazmul Alam <nazmul199512@gmail.com>
2020-06-23 15:27:38 +0600

    Update admin.py

--- job_portal/jobs/admin.py
+++ job_portal/jobs/admin.py
@@ -6,3 +6,12 @@ admin.site.register(JobListing)
 admin.site.register(ApplyJob)
 
 
+class JobListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user')
+    actions = None
+
+    def save_model(self, request, obj, form, change):
+        if not obj.user:
+            obj.user = request.user
+            obj.save()
+
