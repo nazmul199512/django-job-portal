91682746524c90031a19f46a6afed87e4546db1f
MD. Nazmul Alam <nazmul199512@gmail.com>
2020-11-13 14:54:45 +0600

    Update settings.py

--- job_portal/job_portal/settings.py
+++ job_portal/job_portal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '4^fw*wh7gw+ek$ipnlp+#bf1py$$d2vobjddsdfi-7!&5(9v1ifzuu3')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
